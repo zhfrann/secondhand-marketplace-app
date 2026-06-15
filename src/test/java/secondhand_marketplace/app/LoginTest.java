478c8606f6e43f3ac3f31cae704f693aa19e70bd
Mayya <bintangridwanp@gmail.com>
2026-06-15 13:11:27 +0700

    Add simple LoginTest for CI/CD pipeline validation

---
+++ src/test/java/secondhand_marketplace/app/LoginTest.java
@@ -0,0 +1,32 @@
+package secondhand_marketplace.app;
+
+import org.junit.Test;
+
+import secondhand_marketplace.akun.ManajerAkun;
+import secondhand_marketplace.exception.LoginException;
+import secondhand_marketplace.pengguna.Pengguna;
+
+import static org.junit.Assert.*;
+
+public class LoginTest {
+
+    @Test
+    public void testLoginSuccess() throws LoginException {
+        ManajerAkun manajer = new ManajerAkun();
+        Pengguna user = manajer.login("user1", "user1");
+        assertNotNull(user);
+        assertEquals("user10", user.getUsername());
+    }
+
+    @Test(expected = LoginException.class)
+    public void testLoginWrongPassword() throws LoginException {
+        ManajerAkun manajer = new ManajerAkun();
+        manajer.login("user1", "wrongpass");
+    }
+
+    @Test(expected = LoginException.class)
+    public void testLoginUnknownUser() throws LoginException {
+        ManajerAkun manajer = new ManajerAkun();
+        manajer.login("noone", "nopass");
+    }
+}
